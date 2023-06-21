34b76a564a9ddb143177b1db46a9d69b4b3f5cf4
Mehmet Hekimoğlu <mehmet.hekimoglu@kuartismed.com>
2023-06-21 22:24:50 +0300

    Changed variables names for GUI

--- MeteoWidget/GUI/GUI.py
+++ MeteoWidget/GUI/GUI.py
@@ -1,31 +1,50 @@
 import tkinter as tk
 from MeteoWidget.MeteoWidget import MeteoWidget
 
-class MeteoWidgetGUI():
+class MeteoWidgetGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("MeteoWidget")
-
+        
         # Checkbox variables
-        self.checkbox_var1 = tk.BooleanVar()
-        self.checkbox_var2 = tk.BooleanVar()
-        self.checkbox_var3 = tk.BooleanVar()
+        self.hourly_temperature_2m = tk.BooleanVar()
+        self.hourly_relative_humidity_2m = tk.BooleanVar()
+        self.hourly_rain = tk.BooleanVar()
+
+        self.daily_maximum_temperature_2m = tk.BooleanVar()
+        self.daily_minimum_temperature_2m = tk.BooleanVar()
 
         # Latitude and longitude variables
         self.latitude_var = tk.StringVar()
         self.longitude_var = tk.StringVar()
 
-        # Create checkbox
-        self.checkbox_frame = tk.Frame(root)
-        self.checkbox_frame.pack(pady=10)
-        self.checkbox_label = tk.Label(self.checkbox_frame, text="Select items:")
-        self.checkbox_label.pack()
-        self.checkbox_item1 = tk.Checkbutton(self.checkbox_frame, text="Item 1", variable=self.checkbox_var1)
-        self.checkbox_item1.pack()
-        self.checkbox_item2 = tk.Checkbutton(self.checkbox_frame, text="Item 2", variable=self.checkbox_var2)
-        self.checkbox_item2.pack()
-        self.checkbox_item3 = tk.Checkbutton(self.checkbox_frame, text="Item 3", variable=self.checkbox_var3)
-        self.checkbox_item3.pack()
+        # Create Hourly section
+        self.hourly_frame = tk.Frame(root)
+        self.hourly_frame.pack(pady=10)
+
+        self.hourly_label = tk.Label(self.hourly_frame, text="Hourly Weather Variables:")
+        self.hourly_label.pack()
+
+        self.hourly_temperature_2m_checkbox = tk.Checkbutton(self.hourly_frame, text="Temperature (2m)", variable=self.hourly_temperature_2m)
+        self.hourly_temperature_2m_checkbox.pack()
+
+        self.hourly_relative_humidity_2m_checkbox = tk.Checkbutton(self.hourly_frame, text="Relative Humidity (2 m)", variable=self.hourly_relative_humidity_2m)
+        self.hourly_relative_humidity_2m_checkbox.pack()
+
+        self.hourly_rain_checkbox = tk.Checkbutton(self.hourly_frame, text="Rain", variable=self.hourly_rain)
+        self.hourly_rain_checkbox.pack()
+
+
+        # Create Daily section
+        self.daily_frame = tk.Frame(root)
+        self.daily_frame.pack(pady=10)
+
+        self.daily_label = tk.Label(self.daily_frame, text="Daily Weather Variables:")
+        self.daily_label.pack()
+        self.daily_maximum_temperature_2m_checkbox = tk.Checkbutton(self.daily_frame, text="Maximum Temperature (2m)", variable=self.daily_maximum_temperature_2m)
+        self.daily_maximum_temperature_2m_checkbox.pack()
+        self.daily_minimum_temperature_2m_checkbox = tk.Checkbutton(self.daily_frame, text="Minimum Temperature (2m)", variable=self.daily_minimum_temperature_2m)
+        self.daily_minimum_temperature_2m_checkbox.pack()
 
         # Create latitude and longitude entry fields
         self.location_frame = tk.Frame(root)
@@ -45,22 +64,38 @@ class MeteoWidgetGUI():
         self.run_button = tk.Button(self.button_frame, text="Run MeteoWidget", command=self.run_meteo_widget)
         self.run_button.pack()
 
+
+        #Set window dimensions and center it on the screen
+        window_width = 400
+        window_height = 300
+        screen_width =  self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x_coordinate = int((screen_width / 2) - (window_width / 2))
+        y_coordinate = int((screen_height / 2) - (window_height / 2))
+        self.root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+    
+
     def run_meteo_widget(self):
         latitude = float(self.latitude_var.get())
         longitude = float(self.longitude_var.get())
 
-        # Perform actions based on checkbox selections
-        if self.checkbox_var1.get():
-            print("Item 1 selected.")
-            # Perform item 1 action
+        # Perform actions based on Hourly checkbox selections
+        if self.hourly_temperature_2m.get():
+            print("Hourly Option 1 selected.")
+            # Perform Hourly Option 1 action
 
-        if self.checkbox_var2.get():
-            print("Item 2 selected.")
-            # Perform item 2 action
+        if self.hourly_relative_humidity_2m.get():
+            print("Hourly Option 2 selected.")
+            # Perform Hourly Option 2 action
 
-        if self.checkbox_var3.get():
-            print("Item 3 selected.")
-            # Perform item 3 action
+        # Perform actions based on Daily checkbox selections
+        if self.daily_maximum_temperature_2m.get():
+            print("Daily Option 1 selected.")
+            # Perform Daily Option 1 action
+
+        if self.daily_minimum_temperature_2m.get():
+            print("Daily Option 2 selected.")
+            # Perform Daily Option 2 action
 
         # Create MeteoWidget and retrieve forecast
         meteo_widget = MeteoWidget(latitude, longitude)
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import requests
 import tkinter as tk
-#from MeteoWidget import MeteoWidget, MeteoWidgetConsole
 from MeteoWidget.GUI import GUI
-from MeteoWidget.MeteoWidget import  MeteoWidget
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = GUI.MeteoWidgetGUI(root)
     root.mainloop()
-    # widget_console = MeteoWidgetConsole()
-    # widget_console.run()
